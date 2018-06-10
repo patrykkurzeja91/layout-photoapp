f655f4b3ff60407490d6d19adea0c9dec25742a2
Patryk <patrick.kurzeja@gmail.com>
2018-06-10 16:26:45 +0200

    Add styles and basic functions

---
+++ inc/sendEmail.php
@@ -0,0 +1,56 @@
+﻿<?php
+
+// Replace this with your own email address
+$siteOwnersEmail = 'patrick.kurzeja@gmail.com';
+
+
+if($_POST) {
+
+   $email = trim(stripslashes($_POST['contactEmail']));
+   
+   
+	// Check Email
+	if (!preg_match('/^[a-z0-9&\'\.\-_\+]+@[a-z0-9\-]+\.([a-z0-9\-]+\.)*+[a-z]{2}/is', $email)) {
+		$error['email'] = "Please enter a valid email address.";
+	}
+	
+
+
+   // Set Message
+   
+	$message .= "Email address: " . $email . "<br />";
+   $message .= "Add me to subscribe list <br />";
+   $message .= "<br /> ----- <br /> This email was sent from your site's contact form. <br />";
+
+   // Set From: header
+   $from =  " <" . $email . ">";
+
+   // Email Headers
+	$headers .= "Reply-To: ". $email . "\r\n";
+ 	$headers .= "MIME-Version: 1.0\r\n";
+	$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+
+
+   if (!$error) {
+
+      ini_set("sendmail_from", $siteOwnersEmail); // for windows server
+      $mail = mail($siteOwnersEmail, $message, $headers);
+
+		if ($mail) { echo "OK"; }
+      else { echo "Something went wrong. Please try again."; }
+		
+	} # end if - no validation error
+
+	else {
+
+		$response = (isset($error['name'])) ? $error['name'] . "<br /> \n" : null;
+		$response .= (isset($error['email'])) ? $error['email'] . "<br /> \n" : null;
+		$response .= (isset($error['message'])) ? $error['message'] . "<br />" : null;
+		
+		echo $response;
+
+	} # end if - there was a validation error
+
+}
+
+?>
--- js/script.js
+++ js/script.js
@@ -1,211 +1,188 @@
-
 $(document).ready(function () {
+  $("#includePrivacy").load("./src/privacy.html"); 
+  $("#includeTerms").load("./src/ToU.html"); 
+/*----------------------------------------------------*/
+/*	Falling Star 
+------------------------------------------------------*/
+  
   // Adds and removes animation to star every few seconds, also randomly changes its position on screen
-    function LoopAnimate() {
-        // Add animation class to star after 5 secons
-        setTimeout(function() {
-          document.getElementById("mr_star").className += " animate";
-        }, 5000);
-        // remove animation class after 9seconds
-        setTimeout(function() {
-          var star = document.getElementById("mr_star"),
-            starCont = document.getElementById("star-cont");
-      
-          star.className = " star"; 
-          randomMargin(); //add random margin
-        }, 3000);
-      
-      }
-      // Call LoopAnimate every 15seconds
-      LoopAnimate();
-      window.setInterval(function() {
-       LoopAnimate();
-      
-      }, 15000);
-      
-      //function to add a random margin
-      function randomMargin() {
-        var num = Math.floor(Math.random() * 101),
-          starCont = document.getElementById("star-cont"),
-          starContMargin = getComputedStyle(starCont).getPropertyValue("margin-left");
-        num += "%";
-      
-        starCont.style.marginLeft = num;
-        // console.log(num);
-      }
+  function LoopAnimate() {
+    // Add animation class to star after 5 secons
+    setTimeout(function () {
+      document.getElementById("mr_star").className += " animate";
+    }, 5000);
+    // remove animation class after 9seconds
+    setTimeout(function () {
+      var star = document.getElementById("mr_star"),
+        starCont = document.getElementById("star-cont");
 
+      star.className = " star";
+      randomMargin(); //add random margin
+    }, 3000);
 
+  }
+  // Call LoopAnimate every 15seconds
+  LoopAnimate();
+  window.setInterval(function () {
+    LoopAnimate();
+  }, 15000);
 
-      $('.owl-carousel').owlCarousel({
-        navigation: true,
-        loop:true,
-        dots: false,
-        margin:15,
-        responsiveClass:true,
-        slideBy: 2,
-        lazyLoad: true,
-        autoplay: true,
-        autoplayTimeout: 4000,
-        autoplayHoverPause: true,
-        
-        responsive:{
-            0:{
-                items:1,
-                nav:true
-            },
-            400:{
-                items:2,
-                nav:false,
-                nav:true,
-            },
-            
-        }
-    })
-// ======================================================================
-//      My slider
-// ======================================================================
+  //function to add a random margin
+  function randomMargin() {
+    var num = Math.floor(Math.random() * 101),
+      starCont = document.getElementById("star-cont"),
+      starContMargin = getComputedStyle(starCont).getPropertyValue("margin-left");
+    num += "%";
 
+    starCont.style.marginLeft = num;
+    // console.log(num);
+  }
 
-//configuration
-var width = 100,
-animationSpeed = 500,
-pause = 3000;
-//variables to slider
-var sliderWrap = $('.slider-wrapper'),
-inWrap = sliderWrap.find('.inner-wrapper'),
-slides = inWrap.find('.slide'),
-interval;
-
-//setting data-slide attribute to all slides
-for (let i = 0; i < slides.length; i++) {
-const element = slides[i];
-$(element).attr('data-slide', i+1);
-}
-console.log($('.inner-wrapper').html());
-
-//adding pagination to div class="slider-dots"
-const dots = $('.slider-dots').find('li');
-for (let i = 1; i <= slides.length; i++) {
-$('.slider-dots').append('<li><a href="#" data-slide ="' + i + '"></a></li>');
-}
-console.log($('.slider-dots').html());
-//start Interval
-function startSlider() {
-changeDot();
-interval = setInterval(slideRight, pause);
-}
-//stop Interval
-function stopSlider() {
-
-clearInterval(interval);
-}
-//pause on mouseenter (on whole area of slider so Dots as well)
-inWrap.on('mouseenter', stopSlider).on('mouseleave', startSlider);
-//Run animation
-startSlider();
-
-$('.prev').on('click', function (e) {
-e.preventDefault();
-slideLeft();
-stopSlider();
-startSlider();
-
-});
-$('.next').on('click', function (e) {
-e.preventDefault();
-slideRight();
-stopSlider();
-startSlider();
-
-});
-//animation of sliding back
-function slideLeft() {
-toPrev();
-inWrap.stop().animate({
-  marginLeft: 0 + '%'
-}, animationSpeed, 'easeOutSine');
-}
-//animation of sliding next
-function slideRight() {
-inWrap.stop().animate({
-  marginLeft: -width + '%',
-}, animationSpeed, 'easeOutSine', toNext);
-
-}
-//function - insert last slide before first
-function toPrev() {
-var firstSlide = inWrap.find('.slide:first'),
-  lastSlide = inWrap.find('.slide:last');
-inWrap.css('marginLeft', -width + '%');
-firstSlide.before(lastSlide);
-changeDot();
-}
-//function - insert first slide after last
-function toNext() {
-var firstSlide = inWrap.find('.slide:first'),
-  lastSlide = inWrap.find('.slide:last');
-inWrap.css('marginLeft', 0 + '%');
-lastSlide.after(firstSlide);
-changeDot();
-}
-
-
-//pagination
-
-$('.slider-dots a').click(function (e) {
-e.preventDefault();
-let dotLinks = $('.slider-dots').find('a'),
-  firstSlide = inWrap.find('.slide:first'),
-  currentSlide = firstSlide.attr('data-slide'),
-  clickedDot = $(this).attr('data-slide');
-
-  stopSlider();
-
-if (currentSlide < clickedDot) {
-  var slidesCount = clickedDot - currentSlide;
-
-  inWrap.animate({
-    marginLeft: -(slidesCount * (width + '%'))
-  }, animationSpeed,'easeOutSine', function () {
-    for (var i = 0; i < slidesCount; i++) {
-      toNext();
+  $('.owl-one').owlCarousel({
+    navigation: true,
+    loop: true,
+    dots: false,
+    margin: 15,
+    responsiveClass: true,
+    slideBy: 1,
+    lazyLoad: true,
+    autoplay: true,
+    autoplayTimeout: 4000,
+    autoplayHoverPause: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: true
+      },
+      400: {
+        items: 1,
+        nav: false,
+        nav: true,
+      },
 
     }
   });
-} else {
-  var slidesCount = currentSlide - clickedDot;
-  for (var i = 0; i < slidesCount; i++) {
-    toPrev();
-  }
-  inWrap.css({
-    marginLeft: -(slidesCount * (width + '%'))
+  
+  // Owl TWO
+  var owl = $(".owl-two");
+  
+  owl.owlCarousel({
+    navigation: true,
+    loop: true,
+    dots: false,
+    margin: 15,
+    responsiveClass: true,
+    slideBy: 2,
+    lazyLoad: true,
+    autoplay: true,
+    autoplayTimeout: 4000,
+    autoplayHoverPause: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: true
+      },
+      996: {
+        items: 2,
+        nav: false,
+        nav: true,
+      },
+
+    }
   });
-  inWrap.animate({
-    'marginLeft': 0 + '%'
-  }, animationSpeed, 'easeOutSine');
-};
-startSlider();
+
+  
+
+  /*----------------------------------------------------*/
+/*	Modal Popup
+------------------------------------------------------*/
+
+$('.terms').magnificPopup({
+  delegate: 'a',
+  type:'inline',
+  fixedContentPos: false,
+  removalDelay: 200,
+  showCloseBtn: false,
+  mainClass: 'mfp-fade'
+
+});
+
+$(document).on('click', '.popup-modal-dismiss', function (e) {
+   e.preventDefault();
+   $.magnificPopup.close();
 });
 
 
-function changeDot() {
-var currentSlide = inWrap.find(".slide:first");
-var activeDot = $('.slider-dots').find('a');
-var currentSlideNum = currentSlide.attr('data-slide');
-var clickedDot = activeDot.attr('data-slide');
+/*----------------------------------------------------*/
+/* Smooth Scrolling
+------------------------------------------------------ */
 
-activeDot.each(function (index, element) {
-  if ((index + 1) == currentSlideNum) {
-    $(element).addClass('active')
-  } else {
-    $(element).removeClass('active');
-  }
+$('.smoothscroll').on('click',function (e) {
+  e.preventDefault();
+
+  var target = this.hash,
+  $target = $(target);
+
+  $('html, body').stop().animate({
+      'scrollTop': $target.offset().top
+  }, 800, 'swing', function () {
+      window.location.hash = target;
+  });
 });
-}
+  /*----------------------------------------------------*/
+  /*	Make sure that #header-background-image height is
+  /* equal to the browser height.
+  ------------------------------------------------------ */
 
+  $('header').css({
+    'height': $(window).height()
+  });
+  $(window).on('resize', function () {
 
+    $('header').css({
+      'height': $(window).height()
+    });
+    $('body').css({
+      'width': $(window).width()
+    })
+  });
 
+/*----------------------------------------------------*/
+/*subscribe button
+------------------------------------------------------*/
 
-      
+$('#subscribe button.submit').click(function() {
+
+  $('#image-loader').fadeIn();
+
+  var contactEmail = $('#contactEmail').val();
+
+  var data ='&contactEmail=' + contactEmail;
+
+  $.ajax({
+
+    type: "POST",
+    url: "inc/sendEmail.php",
+    data: data,
+    success: function(msg) {
+
+        // Message was sent
+        if (msg == 'OK') {
+           
+           $('#message-warning').hide();
+           $('#contactForm').fadeOut();
+           $('#message-success').fadeIn();   
+        }
+        // There was an error
+        else {
+           
+           $('#message-warning').html(msg);
+          $('#message-warning').fadeIn();
+        }
+
+    }
+
+  });
+  return false;
 });
-
+});
